115e91f4d1985dd64e64906f7eee571d007f83e5
Tien Tien Truong <tientientruong@live.fr>
2022-02-18 15:57:18 -0500

    alignement des svg dans le footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="site_footer">
-    <section class="footer_info">
+    <section class="site_footer_info">
         <ul class="footer_contact">
             <li>3 800, rue Sherbrooke Est Montréal</li>
             <li>(Québec) H1X 2A2</li>
@@ -9,13 +9,20 @@
             <li>Lorem ipsum dolor quaerat?</li>
         </ul>
         <div class="footer_links">
-        <?php wp_nav_menu(array("menu"=>"simple",
-                                "container"=>"nav")); 
+        
+        <?php  
+        $icone = '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#000"><path d="M10 17l5-5-5-5v10z"></path><path d="M0 24V0h24v24H0z" fill="none"></path></svg>';
+        wp_nav_menu(array("menu" => "footer",
+                                "container" => "nav",
+                                "container_class" => "site_footer_menu_nav",
+                                "menu_class" => "footer_menu_ul",
+
+                                "link_before"=> $icone)); 
         ?>
         </div>
         <p class="footer_parag">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui totam consectetur magnam itaque minima soluta ipsa id placeat inventore tempora. Cum voluptate maxime quibusdam amet fuga sed optio. Repudiandae, expedita.</p>
     </section>
-    <section class="footer_droit">
+    <section class="site_footer_droit">
        <p>Copyright &copy 2022 - Collège de Maisonneuve. Tous droits réservés </p>
        
     </section>
