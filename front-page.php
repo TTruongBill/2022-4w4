294e705e90f8b9c76c2bf2ae4d51e24dab35e898
Tien <tientientruong@live.fr>
2022-03-07 00:08:40 -0500

    Ajout de animation du titre

--- front-page.php
+++ front-page.php
@@ -1,14 +1,7 @@
 <?php get_header(); ?>
     <main class="site_main_front_page">
-    <div class="animation">   
-        <h1 class="animation_bloc">1</h1>
-        <h1 class="animation_bloc">2</h1>
-        <h1 class="animation_bloc">3</h1>
-        <h1 class="animation_bloc">4</h1>
-        <h1 class="animation_bloc">5</h1>
-    </div> 
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-        <h1 class="main_titre">
+        <h1 class="main_titre animation_titre">
             <?php the_title();?>
         </h1>
         <?php the_content();?> 
