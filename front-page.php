564210be5ab6bd8ca793e67db474beecbcddca33
Tien Tien Truong <tientientruong@live.fr>
2022-04-09 23:25:20 -0400

    s10-c2 Création de deux blocs de nav dans le front-page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
     <main class="site_main_front_page">
+        <section class="page_important">
+            <div class="dep_TIM">
+
+            </div>
+            <div class="evenement">
+                
+            </div>
+        </section>
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <h1 class="main_titre animation_titre">
             <?php the_title();?>
