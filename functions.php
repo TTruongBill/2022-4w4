1c2c1dd93da94cab1d90c7a5f18a5feafc3e6c97
Tien Tien Truong <tientientruong@live.fr>
2022-04-11 02:04:10 -0400

    s10 -c2 configuration du menu principale dans l'accueil

--- functions.php
+++ functions.php
@@ -116,4 +116,16 @@ function trouve_la_categorie($tableau){
         if(is_category($cle)) return($cle);
     }
 }
+/*----------------Custom logo -----------*/
+function themename_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true, 
+    );
+}
+add_theme_support( 'custom-logo' );
 ?>
