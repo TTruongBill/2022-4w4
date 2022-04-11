3c191335cd66dfced0c9f631291ac985fe424a4e
Tien Tien Truong <tientientruong@live.fr>
2022-04-11 02:12:06 -0400

    s10-c2 Changement du layout de la page accueil, text first, contenu apres

--- front-page.php
+++ front-page.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
     <main class="site_main_front_page">
+        
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <h1 class="main_titre animation_titre">
+            <?php the_title();?>
+        </h1>
+        <section class="animation_contenu">
+            <?php the_content();?> 
+        </section>
+        <?php endwhile;?>
+        <?php endif;?>
         <section class="pages_important">  
                 <h3>Le département TIM</h3>
                 <?php
@@ -12,15 +22,5 @@
                                     "container"=>"nav"));
                 ?>  
         </section>
-        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-        <h1 class="main_titre animation_titre">
-            <?php the_title();?>
-        </h1>
-        <section class="animation_contenu">
-            <?php the_content();?> 
-        </section>
-        <?php endwhile;?>
-        <?php endif;?>
-    
     </main>
 <?php get_footer(); ?>
