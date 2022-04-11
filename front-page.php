4f9a58bf6014e39f08dc29487bb0d48199000580
Tien Tien Truong <tientientruong@live.fr>
2022-04-11 01:26:04 -0400

    s10 c2 Rajout de css au deuxieme menu accueil

--- front-page.php
+++ front-page.php
@@ -1,20 +1,16 @@
 <?php get_header(); ?>
     <main class="site_main_front_page">
         <section class="pages_important">  
-            <div class="bloc_important">
                 <h3>Le département TIM</h3>
                 <?php
                 wp_nav_menu(array('menu'=>"accueil",
                                     "container"=>"nav"));
                 ?>    
-            </div>
-            <div class="bloc_important">
                 <h3>Les événements</h3>
                 <?php
                 wp_nav_menu(array('menu'=>"evenement",
                                     "container"=>"nav"));
                 ?>  
-            </div>
         </section>
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <h1 class="main_titre animation_titre">
