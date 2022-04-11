9a5ae968965ffa6b025404fbca9c3c6387e503ba
Tien Tien Truong <tientientruong@live.fr>
2022-04-11 00:49:02 -0400

    s10-c2 Rajout d'une d'un media screen pour le nav accueil

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ add_theme_support('widgets');
 function prefix_nav_description($item_output, $item) {
     if(!empty($item->description)) {
         $item_output = str_replace( '</a>',
-        '<hr><span class="menu-item-description">' . $item->description . '</span></a>',
+        '<hr><span class="menu-item-description">' . $item->description . '</span><div class="menu-item-icone"></div></a>',
         $item_output);
     }
     return $item_output;
