b7fe9913c6b672784c4492cf3e35a3f2cc19811a
Tien Tien Truong <tientientruong@live.fr>
2022-04-22 19:19:38 -0400

    S12-c2 Rajout d'un field resume et endroit dans le template evenement

--- template-evenement.php
+++ template-evenement.php
@@ -1,17 +1,20 @@
 <?php /*Template Name: Évenement */ ?>
 <?php get_header(); ?>
 <main class="site_main">
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-    <?php the_post_thumbnail(); ?>
-            <h1 class="page_main_titre">
-                <?php the_title();?>
-            </h1>
-            <section class="carte_post">
-                <?php the_content();?>
-            </section> 
-        
-        <?php endwhile;?>
-        <?php endif;?>
+    <article class="evenement">
+    <?php if(have_posts()) : the_post(); ?>
+        <h1 class="page_main_titre">
+            <?php the_title();?>
+        </h1>
+        <section class="carte_post">
+            <?php the_field("resume");?>
+            <p class="evenement_endroit"> 
+                <?php the_field("endroit"); ?>
+            </p>
+
+        </section> 
+    </article>
+    <?php endif;?>
     
 </main>
 <?php get_footer();?>
