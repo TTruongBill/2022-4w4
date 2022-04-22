da99c042920a1af4043a3a4c23802b08ab910591
Tien Tien Truong <tientientruong@live.fr>
2022-04-22 18:59:23 -0400

    s12-c2 Enlèvement du sigle dans la catégorie cours

--- gabarits/content-cours.php
+++ gabarits/content-cours.php
@@ -2,7 +2,6 @@
     $categorie = get_the_category();
     $mon_titre = get_the_title();
     $mon_titre_filtre = substr($mon_titre,8);
-    $mon_sigle = substr($mon_titre,0,7);
     $mon_heure = get_field( "nombre_dheures" ) . " heures";
     $departement = get_field("departement");
     $mon_titre_filtre = substr($mon_titre_filtre, 0, strrpos($mon_titre_filtre,'('));
@@ -16,7 +15,6 @@
         </a>    
     </h3>
     <section class="carte_info">
-        <p class="carte_sigle"><?php echo $mon_sigle; ?></p>
         <p class="carte_heure"><?php echo $mon_heure; ?></p>
     </section>
     <p class="carte_contenu"><?php echo $mon_contenu; ?></p>
