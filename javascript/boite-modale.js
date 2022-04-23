e790522d112e4a1de51e2a4021d7bf267b0c0c17
Tien Tien Truong <tientientruong@live.fr>
2022-04-22 23:20:38 -0400

    s12-c2 Rajout du eventListener pour fermer la boite-modale

--- javascript/boite-modale.js
+++ javascript/boite-modale.js
@@ -2,13 +2,18 @@
 
     let carte_contenu_ouvrir = document.querySelectorAll('.carte_contenu_ouvrir');
     let boite_modale = document.querySelector(".boite_modale");
-
-    console.log(carte_contenu_ouvrir.length);
+    let boite_modale_fermeture = document.querySelector(".boite_modale_fermeture");
+    
+    //console.log(carte_contenu_ouvrir.length);
     for (const bout of carte_contenu_ouvrir){
         bout.addEventListener('mousedown', function(){
-            console.log(this.parentNode.parentNode.className)
+            //console.log(this.parentNode.parentNode.className)
             boite_modale.classList.add('ouvrir')
-            console.log(boite_modale.classList);
+            //console.log(boite_modale.classList);
+        })
+
+        boite_modale_fermeture.addEventListener('mousedown',function(){
+            boite_modale.classList.remove('ouvrir')
         })
     }
 })()
