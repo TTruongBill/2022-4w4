475ecbac6a8e0cd9a04f0557348ed91f24cdaf9f
Tien Tien Truong <tientientruong@live.fr>
2022-04-22 22:01:07 -0400

    Rajout d'un nouveau champ de texte organisateur avec the field dans le template evenement

--- template-evenement.php
+++ template-evenement.php
@@ -1,19 +1,15 @@
 <?php /*Template Name: Évenement */ ?>
 <?php get_header(); ?>
 <main class="site_main">
-    <article class="evenement">
     <?php if(have_posts()) : the_post(); ?>
-        <h1 class="page_main_titre">
-            <?php the_title();?>
-        </h1>
-        <section class="carte_post">
-            <?php the_field("resume");?>
-            <p class="evenement_endroit"> 
-                <?php the_field("endroit"); ?>
-            </p>
-
-        </section> 
-    </article>
+    <h1 class="page_main_titre">
+        <?php the_title();?>
+    </h1>
+    <section class="carte_post evenement">
+        <p class="evenement_resume"><?php the_field("resume");?></p>
+        <p class="evenement_endroit"><?php the_field("endroit");?></p>
+        <p class="evenement_organisateur"><?php the_field('organisateur'); ?></p>
+    </section> 
     <?php endif;?>
     
 </main>
