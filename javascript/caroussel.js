7d837fa67861a1f5d99feed903d500fc48aba8ea
Tien Tien Truong <tientientruong@live.fr>
2022-05-05 20:52:17 -0400

    s14-c2 Début du script caroussel.js

---
+++ javascript/caroussel.js
@@ -0,0 +1,24 @@
+(function(){
+    let boite_modale = document.querySelector(".boite_modale")
+    let galerie_img = document.querySelectorAll('.galerie .wp-block-image img')
+    let boite_modale_ferme = document.querySelector(".boite_modale_ferme")
+    let boite_modale_texte = document.querySelector(".boite_modale_texte")
+  
+    boite_modale_ferme.addEventListener('mousedown', function(){
+      boite_modale.classList.remove('boite_modale-ouvrir')
+    })
+  
+    let image = document.createElement('img')
+    boite_modale_texte.appendChild(image)
+  
+  
+    for (const img of galerie_img) {
+        img.addEventListener('mousedown',function(){
+          boite_modale.classList.add('boite_modale-ouvrir')
+          console.log(this.getAttribute('src'))
+          let source = this.getAttribute('src')
+          image.setAttribute('src',source)
+        })
+    }
+
+  })()
