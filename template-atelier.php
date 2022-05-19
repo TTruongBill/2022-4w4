ae0a500b38c6d2d3caf65806e3c0797f28af1ae6
Tien Tien Truong <tientientruong@live.fr>
2022-05-19 13:35:22 -0400

    ef-TTBT Rajout de append et prepend sur les éléments nécessaire dans le template de l'atelier

--- template-atelier.php
+++ template-atelier.php
@@ -8,17 +8,17 @@
     </h1>
     <h2> Description de l'atelier</h2>
     <section class="carte_post atelier">
-        <p class="atelier_animateur">L'animateur de l'atelier : <b> <?php the_field("animateur");?></b></p>
-        <p class="atelier_local">L'atelier sera donné au local : <b> <?php the_field("local");?></b></p>
+        <p class="atelier_animateur"><?php echo get_field_object("animateur")["prepend"] . " <b> " . get_field_object("animateur")["value"];?></b></p>
+        <p class="atelier_local"><?php echo get_field_object("local")["prepend"] . " <b> " . get_field_object("local")["value"];?></b></p>
         <p class="atelier_description"><?php the_field('description'); ?></p>
     </section> 
     <h2>Horaire et dates de l'atelier</h2>
     <section class="carte_post atelier">
-        <p class="atelier_duree">Durée de chacune des séances et de : <b><?php the_field("duree") ?> heures </b></p>
-        <p class="atelier_date_debut">Date de début : <b> <?php the_field("date_debut") ?></b></p>
-        <p class="atelier_date_fin">Date de fin : <b> <?php the_field("date_fin") ?></b></p>
-        <p class="atelier_jours">la formation se donnera : <b> <?php the_field("jours") ?></b></p>
-        <p class="atelier_heure">L'horaire : de <b> <?php the_field("heure_debut")?></b> à <b> <?php the_field("heure_fin")?></b></p>
+        <p class="atelier_duree"> <?php echo get_field_object("duree")["prepend"] . " <b> " . get_field_object("duree")["value"] . " " . get_field_object("duree")["append"]; ?></b></p>
+        <p class="atelier_date_debut">Date de début : <b> <?php the_field("date_debut"); ?></b></p>
+        <p class="atelier_date_fin">Date de fin : <b> <?php the_field("date_fin"); ?></b></p>
+        <p class="atelier_jours"><?php echo get_field_object("jours")["prepend"] . " <b> " . get_field_object("jours")["value"];?></b></p>
+        <p class="atelier_heure">L'horaire : de <b> <?php the_field("heure_debut")?></b> à <b> <?php the_field("heure_fin");?></b></p>
     </section> 
     <?php endif;?>
     
