5253e1530446444973454d7bf55360446d19d5bc
Tien Tien Truong <tientientruong@live.fr>
2022-05-19 13:03:58 -0400

    s16-EF Rajout de bold pour les messages important dans le template atelier

--- template-atelier.php
+++ template-atelier.php
@@ -8,17 +8,17 @@
     </h1>
     <h2> Description de l'atelier</h2>
     <section class="carte_post atelier">
-        <p class="atelier_animateur">L'animateur de l'atelier : <?php the_field("animateur");?></p>
-        <p class="atelier_local">L'atelier sera donné au local : <?php the_field("local");?></p>
+        <p class="atelier_animateur">L'animateur de l'atelier : <b> <?php the_field("animateur");?></b></p>
+        <p class="atelier_local">L'atelier sera donné au local : <b> <?php the_field("local");?></b></p>
         <p class="atelier_description"><?php the_field('description'); ?></p>
     </section> 
     <h2>Horaire et dates de l'atelier</h2>
     <section class="carte_post atelier">
-        <p class="atelier_duree">Durée de chacune des séances et de : <?php the_field("duree") ?> heures</p>
-        <p class="atelier_date_debut">Date de début : <?php the_field("date_debut") ?></p>
-        <p class="atelier_date_fin">Date de fin : <?php the_field("date_fin") ?></p>
-        <p class="atelier_jours">la formation se donnera : <?php the_field("jours") ?></p>
-        <p class="atelier_heure">L'horaire : de <?php the_field("heure_debut")?> à <?php the_field("heure_fin")?></p>
+        <p class="atelier_duree">Durée de chacune des séances et de : <b><?php the_field("duree") ?> heures </b></p>
+        <p class="atelier_date_debut">Date de début : <b> <?php the_field("date_debut") ?></b></p>
+        <p class="atelier_date_fin">Date de fin : <b> <?php the_field("date_fin") ?></b></p>
+        <p class="atelier_jours">la formation se donnera : <b> <?php the_field("jours") ?></b></p>
+        <p class="atelier_heure">L'horaire : de <b> <?php the_field("heure_debut")?></b> à <b> <?php the_field("heure_fin")?></b></p>
     </section> 
     <?php endif;?>
     
